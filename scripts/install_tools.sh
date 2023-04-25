3550c456f94231bff4624701d3933547d3c154ca
UTDoi <doisaltu1899@gmail.com>
2023-04-25 13:57:37 +0900

    linuxbrewを使わないように

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -6,23 +6,27 @@ source $DOTFILES_DIR/scripts/utils.sh
 source $DOTFILES_DIR/scripts/install_brew_packages.sh
 source $DOTFILES_DIR/scripts/setup_mac_os_config.sh
 
-if !(is_exists "brew"); then
-  log_info "installing Homebrew ..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if is_darwin; then
+  if !(is_exists "brew"); then
+    log_info "installing Homebrew ..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  # for M1
-  if [[ "$(arch)" == "arm64" ]]; then
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+    # for M1
+    if [[ "$(arch)" == "arm64" ]]; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+  fi
+
+  log_info "installing brew packages ..."
+  install_brew_packages
+
+  if [ $(echo $SHELL) != $(brew --prefix)/bin/zsh ]; then
+    echo $(brew --prefix)/bin/zsh | sudo tee -a /etc/shells
+    sudo chsh -s $(which zsh) && true
   fi
 fi
 
-log_info "installing brew packages ..."
-install_brew_packages
-
-if [ $(echo $SHELL) != $(brew --prefix)/bin/zsh ]; then
-  echo $(brew --prefix)/bin/zsh | sudo tee -a /etc/shells
-  sudo chsh -s $(which zsh) && true
-fi
+# aptで必要なツールを持ってくる処理
 
 if [ ! -d ~/.zinit ]; then
   mkdir ~/.zinit
@@ -35,9 +39,7 @@ if is_darwin; then
   do
     code --install-extension $line
   done
-fi
 
-if is_darwin; then
   log_info "setup mac os config ..."
   setup_mac_os_config
 
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -2,8 +2,3 @@
 if [[ "$(arch)" == "arm64" ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
-
-# for linuxbrew
-if [[ "$(uname -s)" == 'Linux' ]]; then
-  eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-fi
